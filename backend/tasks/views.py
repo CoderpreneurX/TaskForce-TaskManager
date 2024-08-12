de5ba7a263850e7056566148c0a299e03a7356bd
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-13 00:47:18 +0530

    Added CRUD API endpoints for Task and Task Comments

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task, TaskComment
+from .serializers import TaskSerializer, TaskCommentSerializer
 
-# Create your views here.
+class CreateTaskAPIView(generics.CreateAPIView):
+    queryset = Task.objects.all().order_by('-updated_at')
+    serializer_class = TaskSerializer
+
+class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskCommentListCreateView(generics.ListCreateAPIView):
+    queryset = TaskComment.objects.all()
+    serializer_class = TaskCommentSerializer
+
+class TaskCommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TaskComment.objects.all()
+    serializer_class = TaskCommentSerializer

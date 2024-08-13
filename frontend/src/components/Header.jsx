fc14ed4171ba724485d746ebdecc60f2514ce70b
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-13 09:43:58 +0530

    Changed project logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='px-4 md:border-x-2 md:border-t-2 md:rounded md:mt-2 sticky top-0 bg-secondary py-2 border-b-4 border-black'>
-      <h1 className='text-2xl font-bold'>Noteopia</h1>
+      <h1 className='text-2xl font-bold'>TaskForce</h1>
     </header>
   )
 }

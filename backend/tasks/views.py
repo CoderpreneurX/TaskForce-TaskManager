1fed5940adb1d0390f6b810c56d9911e96ce32db
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-13 10:22:10 +0530

    Updated Create task API view, obtain user from request header

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,10 +1,18 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Task, TaskComment
 from .serializers import TaskSerializer, TaskCommentSerializer
 
 class CreateTaskAPIView(generics.CreateAPIView):
     queryset = Task.objects.all().order_by('-updated_at')
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(creator=user)
+        return serializer
 
 class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()

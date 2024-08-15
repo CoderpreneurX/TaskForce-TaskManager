4071cdb9ad20196110bde9269aec9f55b63b683e
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-15 10:00:21 +0530

    Set With Credentials header to false for API requests

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -5,7 +5,7 @@ const baseURL = import.meta.env.VITE_API_BASE_URL
 
 export const client = axios.create({
     baseURL: baseURL,
-    withCredentials: true,
+    // withCredentials: true,
 })
 
 client.interceptors.request.use((config) => {

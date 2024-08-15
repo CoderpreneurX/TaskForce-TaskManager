fb903b1f37bf5683b056f16597e5c38355165aed
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-15 10:01:13 +0530

    Updated the site routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
 import LoginPage from './pages/LoginPage'
-import NoteDetailView from './pages/NoteDetailView'
-import CreateNote from './pages/CreateNote'
-import UpdateNote from './pages/UpdateNote'
 import ProtectedRoute from './components/ProtectedRoute'
+import CreateTask from './pages/CreateTask'
+import AddMembersForm from './components/AddMembersForm'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={
-          <ProtectedRoute>
-            <Notes />
-          </ProtectedRoute>
-        } />
+        <Route path='/' element={<AddMembersForm />} />
         <Route path='/login/' element={<LoginPage />} />
-        <Route path='/view-note/:id/' element={
-          <ProtectedRoute>
-            <NoteDetailView />
-          </ProtectedRoute>
-        } />
         <Route path='/create-note/' element={
           <ProtectedRoute>
-            <CreateNote />
-          </ProtectedRoute>
-        } />
-        <Route path='/update-note/:id/' element={
-          <ProtectedRoute>
-            <UpdateNote />
+            <CreateTask />
           </ProtectedRoute>
         } />
       </Routes>

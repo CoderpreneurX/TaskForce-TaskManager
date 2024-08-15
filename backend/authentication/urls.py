2da623bb78410ca2ab9f48c1469174e2805cfe65
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-15 09:55:27 +0530

    Added url for list users API view

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterUserView, CreateUserProfileView, EditUserProfileView, RetrieveUserProfileView
+from .views import RegisterUserView, CreateUserProfileView, EditUserProfileView, RetrieveUserProfileView, ListUsersAPIView
 
 urlpatterns = [
     path('user/register/', RegisterUserView.as_view(), name='register-user'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('user/profile/register/', CreateUserProfileView.as_view(), name='create-user-profile'),
     path('user/profile/edit/', EditUserProfileView.as_view(), name='edit-user-profile'),
     path('user/profile/', RetrieveUserProfileView.as_view(), name='retrieve-user-profile'),
+    path('list-users/', ListUsersAPIView.as_view(), name='list-users'),
 ]

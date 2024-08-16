9473ccc8eb446244b8774d56ede7758c7b7a9394
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-16 17:38:52 +0530

    Added more routes for CRUD operations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,33 @@ import LoginPage from './pages/LoginPage'
 import ProtectedRoute from './components/ProtectedRoute'
 import CreateTask from './pages/CreateTask'
 import AddMembersForm from './components/AddMembersForm'
+import ViewTask from './pages/ViewTask'
+import Homepage from './pages/Homepage'
+import EditTask from './pages/EditTask'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<AddMembersForm />} />
+        <Route path='/' element={
+          <ProtectedRoute>
+            <Homepage />
+          </ProtectedRoute>
+        } />
         <Route path='/login/' element={<LoginPage />} />
-        <Route path='/create-note/' element={
+        <Route path='/create-task/' element={
           <ProtectedRoute>
             <CreateTask />
+          </ProtectedRoute>
+        } />
+        <Route path='/view-task/:taskId/' element={
+          <ProtectedRoute>
+            <ViewTask />
+          </ProtectedRoute>
+        } />
+        <Route path='/edit-task/:taskId/' element={
+          <ProtectedRoute>
+            <EditTask />
           </ProtectedRoute>
         } />
       </Routes>

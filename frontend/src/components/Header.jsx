6bcd500794bcd1754e72502234df5ac7dcd21820
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-16 17:37:45 +0530

    Updated UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className='px-4 md:border-x-2 md:border-t-2 md:rounded md:mt-2 sticky top-0 bg-secondary py-2 border-b-4 border-black'>
+    <header className='px-4 sticky top-0 bg-secondary py-2 border-b-4 border-black'>
       <h1 className='text-2xl font-bold'>TaskForce</h1>
     </header>
   )

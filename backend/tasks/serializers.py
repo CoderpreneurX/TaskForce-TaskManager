a2394aac09317b47380e252587ed562701866118
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-16 17:33:04 +0530

    Altered Serializers to retrieve fields with complete details

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,18 +1,24 @@
 from .models import Task, TaskComment
 from rest_framework.serializers import ModelSerializer
+from authentication.serializers import UserSerializer
 
-class TaskSerializer(ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
-        extra_kwargs = {
-            'creator': {'required': False}
-        }
 
 class TaskCommentSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
-        model = TaskSerializer
-        fields = '__all__'
+        model = TaskComment
+        fields = "__all__"
         extra_kwargs = {
-            'user': {'required': False}
+            "user": {"required": False},
+            "task": {"required": False},
         }
+
+
+class TaskSerializer(ModelSerializer):
+    comments = TaskCommentSerializer(many=True, read_only=True)
+    assigned_members = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+        extra_kwargs = {"creator": {"required": False}}

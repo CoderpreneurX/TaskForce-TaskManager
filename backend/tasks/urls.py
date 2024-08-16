dec3d3b96c0b7b0175399f43bb4d92279fbe8b3c
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-16 17:33:23 +0530

    Added List tasks view

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateTaskAPIView, TaskRetrieveUpdateDestroyView, TaskCommentListCreateView, TaskCommentRetrieveUpdateDestroyView
+from .views import CreateTaskAPIView, ListTasksAPIView, TaskRetrieveUpdateDestroyView, TaskCommentListCreateView, TaskCommentRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("create-task/", CreateTaskAPIView.as_view(), name="create-task"),
+    path('list-tasks/', ListTasksAPIView.as_view(), name="list-tasks"),
     path(
         "retrieve-update-delete-task/<uuid:pk>/",
         TaskRetrieveUpdateDestroyView.as_view(),

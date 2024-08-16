f78d38fe2f9849b1b9767de558626151c0ad2690
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-16 17:38:13 +0530

    Added redirect to homepage on task create

--- frontend/src/pages/CreateTask.jsx
+++ frontend/src/pages/CreateTask.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import CreateUpdateTaskForm from '../components/CreateUpdateTaskForm'
 import {client} from '../utils/api'
+import { useNavigate } from 'react-router-dom'
 
 export default function CreateTask() {
+  const navigate = useNavigate()
   function handleFormSubmit(formData) {
     client.post('taskforce/create-task/', formData).then((response) => {
       console.log(response)
+      confirm('Task added successfully!')
+      navigate('/')
     }).catch((error) => {
       console.error(error.response)
     })
   }
 
   return (
-    <>
+    <section className='min-h-screen p-4 flex'>
       <CreateUpdateTaskForm onSubmit={(fd) => {handleFormSubmit(fd)}} />
-    </>
+    </section>
   )
 }
